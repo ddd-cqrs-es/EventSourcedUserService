cac83fd912174d4e38d0d98b503f410f5a94304b
Robert Macdonald Smith <rmacdonaldsmith@yahoo.co.uk>
2014-01-16 14:07:02 -0600

    get my .gitignore in shape

---
+++ UserService.DomainModel/RestaurantId.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace UserService.DomainModel
+{
+    public class RestaurantId : IEquatable<RestaurantId>
+    {
+        private readonly Guid _id;
+
+        public Guid Gpid
+        {
+            get { return _id; }
+        }
+
+        public RestaurantId(Guid gpid)
+        {
+            _id = gpid;
+        }
+
+        public bool Equals(RestaurantId other)
+        {
+            return _id.Equals(other._id);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            return obj is RestaurantId && Equals((RestaurantId)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return _id.GetHashCode();
+        }
+
+        public static implicit operator Guid(RestaurantId id)
+        {
+            return id._id;
+        }
+    }
+}
---
+++ UserService.DomainModel/WishListItem.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using UserService.DomainModel.Events;
+
+namespace UserService.DomainModel
+{
+    public class WishListItem : Entity
+    {
+        public WishListItemId Id { get; private set; }
+        public RestaurantId RestoId { get; private set; }
+
+        public WishListItem(Action<object> applier) 
+            : base(applier)
+        {
+            Register<UserEvents.UserHasNewWishListItem>(When);
+        }
+
+        protected void When(UserEvents.UserHasNewWishListItem newWishListItem)
+        {
+            Id = new WishListItemId(newWishListItem.WishListItemId);
+            RestoId = new RestaurantId(newWishListItem.RestaurantId);
+        }
+    }
+
+    public struct WishListItemId : IEquatable<WishListItemId>
+    {
+        readonly Guid _value;
+
+        public WishListItemId(Guid value)
+        {
+            _value = value;
+        }
+
+        public bool Equals(WishListItemId other)
+        {
+            return _value.Equals(other._value);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            return obj is WishListItemId && Equals((WishListItemId)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return _value.GetHashCode();
+        }
+
+        public static implicit operator Guid(WishListItemId id)
+        {
+            return id._value;
+        }
+    }
+}
